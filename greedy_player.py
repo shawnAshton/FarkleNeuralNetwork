c2a4dfd682a2fbd9d5fb5b14cdd9ab32fad398be
shawnAshton <ash14009@byui.edu>
2018-12-06 21:10:05 -0700

    a

--- greedy_player.py
+++ greedy_player.py
@@ -24,26 +24,61 @@ class Player:
         return freeze_dice
 
     def train(self, sess):
-        # model -> nn
-        # remove underscore in memory
-        batch = self._memory.sample(self._model.batch_size)
-        states = np.array([val[0] for val in batch])
-        next_states = np.array([(np.zeros(self._model.num_states)
-                                 if val[3] is None else val[3]) for val in batch])
-        q_s_a = self._model.predict_batch(states, self._sess)
-        q_s_a_d = self._model.predict_batch(next_states, self._sess)
-        x = np.zeros((len(batch), self._model.num_states))
-        y = np.zeros((len(batch), self._model.num_actions))
+        # note: model = nn
+        # toDO: remove underscore in memory
+        batch = self._memory.sample(self._model.batch_size) # samples your memory... then trains based on what it knows...
+        # states = np.array([val[0] for val in batch])
+        # next_states = np.array([(np.zeros(self._model.num_states)
+        #                          if val[3] is None else val[3]) for val in batch])
+        # q_s_a = self._model.predict_batch(states, self._sess)
+        # q_s_a_d = self._model.predict_batch(next_states, self._sess)
+        inputs = np.zeros((len(batch), self._model.num_states))
+        outputs = np.zeros((len(batch), self._model.num_actions))
         for i, b in enumerate(batch):
             state, frozen_dice, cScore, pScore = b[0], b[1], b[2], b[3]
-            current_q = q_s_a[i]
+            # current_q = q_s_a[i]
+
+
+
             if pScore is None:
                 pScore = cScore
-                current_q[frozen_dice] = cScore
+                # current_q[frozen_dice] = cScore
             else:
-                current_q[frozen_dice] = pScore
+                if pScore <= cScore:
+                    outputs[i] = [0, 0, 0, 0, 0, 0]
+                else:
+                #     find the number of tings we can actually reroll... + 1
+                    count_frozen_die = 0
+                    for die in frozen_dice:
+                        if die is 0:
+                            count_frozen_die += 1
+                    count_frozen_die += 1
+                    amount_to_reroll = 6 - count_frozen_die
+                    single_target = [1,1,1,1,1,1]
+                    count_of_target_frozen = 0
+                    for j, die in enumerate(state):
+                        if frozen_dice[j] == 0:
+                            single_target[j] = 0
+                            count_of_target_frozen += 1
+                        elif die is 1 and count_of_target_frozen < amount_to_reroll:
+                            single_target[j] = 0
+                            count_of_target_frozen += 1
+
+                    for j, die in enumerate(state):
+                        if frozen_dice[j] == 0:
+                            single_target[j] = 0
+                            count_of_target_frozen += 1
+                        elif die is 5 and count_of_target_frozen < amount_to_reroll:
+                            single_target[j] = 0
+                            count_of_target_frozen += 1
+
+
+                # current_q[frozen_dice] = pScore
+
+
+
         #    THINKING OF current scores and potential scores... if statements..? looking at the replay function of 2nd link
-        #
+        #  also... we are here cause we think we need to add to memory b4 we can decide...
         # pytorch..?
 
 
@@ -51,7 +86,7 @@ class Player:
 
 
 
-
+        # self.neuralNet.train_batch(sess,states,rewards)
 
 
 
