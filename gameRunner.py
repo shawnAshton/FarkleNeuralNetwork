1e6a093984e80f549596ec09817e3093e696fb71
shawnAshton <ash14009@byui.edu>
2018-12-15 15:34:06 -0700

    THIS IS GOOD

--- gameRunner.py
+++ gameRunner.py
@@ -16,6 +16,7 @@ class GameRunner:
         self.learning_rate_min = learning_rate_min
 
     def run(self):
+        counted_once = False
         playing = True
         count_of_same_predictions = 0
         count_of_predictions_in_first_50_games = 0
@@ -42,7 +43,7 @@ class GameRunner:
                 # print(self.player.gameScore)
                 valid = False
                 # The player makes a decision:
-                reRoll_test = self.player.perfect_roll(self.farkle.dice)
+                reRoll_test = self.player.perfect_roll(self.farkle.dice, self.farkle.reRoll)
                 reRoll = self.player.decide(self.farkle.dice, self.farkle.reRoll, self.tensor_session)
                 count_of_total_predictions += 1
                 if reRoll_test == reRoll:
@@ -57,7 +58,6 @@ class GameRunner:
                     count_of_decisions_first_3_games += 1
 
                 while not valid:
-                    wrong_answer = False
                     actual_final_round_score = self.farkle.score_roll(self.farkle.dice, self.farkle.reRoll)
                     valid = self.farkle.is_valid_move(reRoll)  # player... use brain... THIS CHANGES REROLL IF ALL ZEROS
                     if (valid) and (not any(reRoll)):
@@ -67,14 +67,16 @@ class GameRunner:
                         if valid:
                            self.farkle.set_reRoll(reRoll)
                         else:
-                            wrong_answer = True
-                            count_of_times_needing_help += 1
-                            if game_count < 3:
-                                count_of_help_needed_first_3_games += 1
-                            if game_count >= 498:
-                                count_of_help_needed_last_3_games += 1
-                            reRoll = self.farkle.randomize_frozen()
 
+                            if not counted_once:
+                                count_of_times_needing_help += 1
+                                counted_once = True
+                                if game_count < 3:
+                                    count_of_help_needed_first_3_games += 1
+                                if game_count >= 498:
+                                    count_of_help_needed_last_3_games += 1
+                            reRoll = self.player.perfect_roll(self.farkle.dice, self.farkle.reRoll)
+                counted_once = False
                 roll_score = self.farkle.score_roll(self.farkle.dice, self.farkle.reRoll)
                 temp_memory = [self.farkle.dice, self.farkle.reRoll]
 
@@ -83,9 +85,6 @@ class GameRunner:
 
                 # Triggers if all the dice zero
                 if self.farkle.new_round:
-                    if wrong_answer:
-                        print("im here")
-                        actual_final_round_score = 0
                     # print("actual_final_round_score ", actual_final_round_score)
                     final_round_score = self.player.roundScore + roll_score
                     round_scores.append(actual_final_round_score)
@@ -123,28 +122,32 @@ class GameRunner:
                 playing = False
         average_round_score_per_game = [plot_game_score[i] / plot_number_rounds[i] for i in range(len(plot_game_score))]
         # average_round_score_per_game = median(plot_game_score)
-        print("in the first 3 rounds i needed help: ", count_of_help_needed_first_3_games , " when I made this many decisions...", count_of_decisions_first_3_games)
-        print("in the last 3 rounds i needed help: ", count_of_help_needed_last_3_games, " when I made this many decisions...", count_of_decisions_last_3_games)
-        print("times needed help in order to function, ", count_of_times_needing_help)
-        print("count_of_predictions_in_first_50_games: ", count_of_predictions_in_first_50_games)
-        print("count_of_predictions_in_last_50_games: ", count_of_predictions_in_last_50_games)
+        print("in the first 3 rounds i needed help: ", count_of_help_needed_first_3_games, " when I made this many decisions...",
+              count_of_decisions_first_3_games, "as percent", count_of_help_needed_first_3_games / count_of_decisions_first_3_games * 100)
+
+        print("in the last 3 rounds i needed help: ", count_of_help_needed_last_3_games, " when I made this many decisions...",
+              count_of_decisions_last_3_games, "or as a percent", count_of_help_needed_last_3_games / count_of_decisions_last_3_games * 100)
+        print(count_of_times_needing_help, " / ", count_of_total_predictions,
+              "times needed help in order to function as a percent, ", count_of_times_needing_help / count_of_total_predictions * 100)
+
+        # print("count_of_predictions_in_first_50_games: ", count_of_predictions_in_first_50_games)
+        # print("count_of_predictions_in_last_50_games: ", count_of_predictions_in_last_50_games)
         print("Count of same predictions is: ", count_of_same_predictions, " / ", count_of_total_predictions,
               " or as a percent ", count_of_same_predictions / count_of_total_predictions * 100)
-        print("median game score", median(plot_game_score))
-        plt.plot(average_round_score_per_game)
-        plt.title("Average round score per game")
-        plt.ylabel("Average Round Score")
-        plt.xlabel("Game Number")
-        plt.show()
-        plt.plot(plot_number_rounds)
-        plt.title("Number of Rounds per Game")
-        plt.ylabel("Number of Rounds")
-        plt.xlabel("Game Number")
-        plt.show()
+        # plt.plot(average_round_score_per_game)
+        # plt.title("Average round score per game")
+        # plt.ylabel("Average Round Score")
+        # plt.xlabel("Game Number")
+        # plt.show()
+        # plt.plot(plot_number_rounds)
+        # plt.title("Number of Rounds per Game")
+        # plt.ylabel("Number of Rounds")
+        # plt.xlabel("Game Number")
+        # plt.show()
         # plot.plot(plot_game_score)
         # plot.title("Game score over time")
         # plot.ylabel("Game Score")
         # plot.xlabel("Turn")
         # plot.show()
         print("Average roll_score per game with NN: ", str(total_score / count_of_total_predictions), " the num of games played: ", game_count)
-        print("median round score with NN", median(round_scores))
+        # print("median round score with NN", median(round_scores))
